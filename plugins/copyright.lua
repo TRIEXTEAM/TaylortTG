c6b1b6f689c2b7aa51de4a4ff52c407ad93ff25c
TRIEXTEAM <triexteam2016@gmail.com>
2016-03-25 19:35:25 +0430

    Update copyright.lua

--- plugins/copyright.lua
+++ plugins/copyright.lua
@@ -1,25 +1,25 @@
 do
 
 local function run(msg, matches)
-  if matches[1]:lower() == 'telemanager' or 'telemanagerplus' then --change this with anything you want
+  if matches[1]:lower() == 'taylor' or 'taylortg' then --change this with anything you want
     send_document(get_receiver(msg), "./data/me/telemanager.webp", ok_cb, false) --put here sticker location(if you want)
       return [[ 
   
-TeleManagerPlus (Open Source) V 3.0.0
+TayorTG (Open Source) V 2.0.0
     Advanced ManagerBot Based On TeleSeed
     
     Edited By : @ali_ghoghnoos
     
     Our Telegram Channel ID :
-    @Telemanager_ch
+    @TaylorChannel
   
     Speacial Thanks To My friends :
-    @ThisIsArman
-    @minaco
-    @dragon_born
+    @MehrPourya
+    @ParsaAlemi
+    @HosseinTriex
     and other that help me for this Bot
-     GitHub :
-     Https://github.com/telemanagerplus/TeleManagerPlus
+     
+   
 
 
  ]]
@@ -28,10 +28,10 @@ end
 
 return {
   patterns = {
-    "^[!/]([Tt]elemanager)$",
-    "^([Tt]elemanager)$",
-    "^[!/]([Tt]elemanagerplus)$",
-    "^([Tt]elemanagerplus)$",
+    "^[!/]([Tt]aylor)$",
+    "^([Tt]aylor)$",
+    "^[!/]([Tt]aylortg)$",
+    "^([Tt]aylortg)$",
     "^(@telemanagerplus)$",
     
     },
